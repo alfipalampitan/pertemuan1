400ad7d433a01f2b69e49658373ed078694da05e
alfipalampitan <alfipalampitan123@gmail.com>
2024-10-03 14:30:44 +0800

    iniTugasVariable

---
+++ bin/pertemuan1/VariableSemua.dart
@@ -0,0 +1,54 @@
+// INI ADALAH DEKLARASI VARIABLE SECARA LANGSUNG
+// void main(){
+//   String nama = "Muhammad Alfianor Fitri";
+
+//   print(nama);
+//   print(nama);
+//   print(nama);
+// }
+
+
+// INI ADALAH KATA KUNCI VAR
+// void main(){
+//   var nama = "Muhammad Alfianor Fitri";
+//   print(nama);
+// }
+
+
+//INI ADALAH KATA KUNCI FINAL
+// void main(){
+//   var namaPertama = "Muhammad";
+//   final namaTerakhir = "Alfianor Fitri";
+
+//   namaPertama = "Muhammad Alfi";
+//   namaTerakhir = "Anor Fitri";
+
+//   print(namaPertama);
+//   print(namaTerakhir);
+// }
+
+
+// INI ADALAH KATA KUNCI CONST
+// void main(){
+//   final array1 = [3, 4, 5];
+//   const array2 = [3, 4, 5];
+
+//   array1[1] = 5;
+//   array2[1] = 6;
+
+
+//   print(array1);
+//   print(array2);
+// }
+
+
+// INI ADALAH KATA KUNCI LATE
+// void main(){
+//   late var value = getValue();
+//   print("variable dibuat");
+//   print(value);
+// }
+// String getValue() {
+//   print("getValue() dipanggil");
+//   return 'muhammad Alfianor Fitri';
+//   }
