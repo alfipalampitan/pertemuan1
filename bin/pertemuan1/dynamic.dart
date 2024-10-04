870b674b2070c0d47eafca57eb8e3ad8039cbee1
alfipalampitan <alfipalampitan123@gmail.com>
2024-10-04 17:14:07 +0800

    ini tugas Dynamic pa

---
+++ bin/pertemuan1/dynamic.dart
@@ -0,0 +1,14 @@
+//INI ADALAH DYNAMIC DI MANA GUNA NYA AGAR BISA MENAMPUNG SEMUA JENIS TIPE DATA
+// void main(){
+//   dynamic variabel = 20;
+//   print(variabel);
+
+//   variabel = true;
+//   print(variabel);
+
+//   variabel = 3.14;
+//   print(variabel);
+
+//   variabel = 'Alfi';
+//   print(variabel);
+// }
