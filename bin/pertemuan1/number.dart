0e960ff4d2952f13ed46e9bf749ae88a98aa4c02
alfipalampitan <alfipalampitan123@gmail.com>
2024-10-04 16:32:27 +0800

    ini tugas untuk number pa

---
+++ bin/pertemuan1/number.dart
@@ -0,0 +1,18 @@
+// INI ADALAH CONTOH UNTUK NUMBER
+// void main() {
+//   int number1 = 10;
+//   double number2 = 3.14;
+
+//   print(number1);
+//   print(number2);
+// }
+
+
+//INI ADALAH CONTOH DARI NUM
+// void main(){
+//   num number = 10;
+//   print(number);
+
+//   number = 3.14;
+//   print(number);
+// }
