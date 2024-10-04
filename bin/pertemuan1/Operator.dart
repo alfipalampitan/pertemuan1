3aeeb5aea2af4aabc5cbeb2a304178a3e7f35b79
alfipalampitan <alfipalampitan123@gmail.com>
2024-10-04 20:18:16 +0800

    ini tugas operator pa ulun gabung jadi satu

---
+++ bin/pertemuan1/Operator.dart
@@ -0,0 +1,102 @@
+//INI ADALAH OPERATOR ARITMATIKA
+// void main(){
+//   var hasil1 = 10 + 20;
+//   var hasil2 = 11 - 3;
+//   var hasil3 = -20;
+//   var hasil4 = 12 * 3;
+//   var hasil5 = 10 / 6;
+//   var hasil6 = 10 ~/ 6;
+//   var hasil7 = 19 % 5;
+
+//   print(hasil1);
+//   print(hasil2);
+//   print(hasil3);
+//   print(hasil4);
+//   print(hasil5);
+//   print(hasil6);
+//   print(hasil7);
+// }
+
+
+//INI ADALAH OPERATOR PERBANDINGAN
+// void main(){
+//   print(10 == 10);
+//   print(10 != 10);
+//   print(10 < 10);
+//   print(10 > 10);
+//   print(10 <= 10);
+//   print(10 >= 10);
+
+//   print('alfi' == 'alfi');
+//   print('alfi' != 'alfi');
+// }
+
+//INI ADALAH OPERATOR PENUGASAN (karena banyak jadi ulun contohkan 2 aja)
+// void main(){
+//   var a = 5;
+
+//   a= a + 5;
+
+//   a += 5;
+
+//   var b = 7;
+
+//   b -= 7;
+
+//   print(a);
+//   print(b);
+// }
+
+
+//INI ADALAH INCREMENT DAN DECREMENT
+// void main(){
+//   var i = 0;
+//   // i++;
+
+//   var j = i++; //i nya dulu baru di tambah 1 jadi j itu = i dan i itu sama dengan 0
+//   var K = ++i; // i nya di tambah dulu baru di tampilkan
+//   print(i);
+//   print(j);
+//   print(K);
+// }
+
+
+//INI ADALAH OPERATOR LOGIKA
+// void main(){
+//   var nilaiAkhir = 90;
+//   var nilaiTugas = 50;
+
+//   var apakahNilaiAkhirBagus = nilaiAkhir >= 75;
+//   var apakahNilaiTugasBagus = nilaiTugas >= 75;
+
+//   print(apakahNilaiAkhirBagus);
+//   print(apakahNilaiTugasBagus);
+
+//   var lulus = apakahNilaiAkhirBagus && apakahNilaiTugasBagus;
+//   var lulus2 = apakahNilaiAkhirBagus || apakahNilaiTugasBagus;
+
+//   print(lulus);
+
+
+//   print(!true);
+//   print(!false);
+// }
+
+//INI ADALAH OPERATOR TYPE TEST
+// void main(){
+//   dynamic variable = 100;
+
+//   // var variableString = variable as String;
+//   var variableInt = variable as int;
+
+//   print(variable);
+//   print(variableInt);
+
+//   print(variable is int);
+//   print(variable is bool);
+//   print(variable is String);
+
+//   print(variable is! int);
+//   print(variable is! bool);
+//   print(variable is! String);
+// }
