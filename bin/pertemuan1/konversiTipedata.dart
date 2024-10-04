207c436358c64954197217d8ae3731e3ba92ff21
alfipalampitan <alfipalampitan123@gmail.com>
2024-10-04 17:49:27 +0800

    ini untuk tugas konversi tipe data pa

---
+++ bin/pertemuan1/konversiTipedata.dart
@@ -0,0 +1,22 @@
+//INI ADALAH CONTOH KONVERSI TIPEDATA INT,DOUBLE,DAN STRING
+// void main(){
+//   var masukkanstring = '100';
+//   var masukanInt = int.parse(masukkanstring);
+//   var masukanDouble = double.parse(masukkanstring);
+
+//   var doubledariInt = masukanInt.toDouble();
+//   var intdariDouble = masukanDouble.toInt();
+
+//   var stringdariInt = masukanInt.toString();
+//   var stringdariDouble = masukanDouble.toString();
+// }
+
+//INI ADALAH CONTOH KONVERSI TIPEDATA BOOLEAN KE STRING
+// void main(){
+//   var masukkanstring = 'true';
+//   var masukanBoolean = masukkanstring == 'true';
+
+//   var stringdariBoolean = masukanBoolean.toString();
+
+//   print(stringdariBoolean);
+//   }
