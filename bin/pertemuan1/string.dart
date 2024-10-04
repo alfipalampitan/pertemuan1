f3ee27671fde96a6b275c0a9719fdbf54c5a5252
alfipalampitan <alfipalampitan123@gmail.com>
2024-10-04 17:08:05 +0800

    ini tugas String pa

---
+++ bin/pertemuan1/string.dart
@@ -0,0 +1,47 @@
+//INI CONTOH PENGGUNAAN TIPE DATA STRING
+// void main(){
+//   String namaAwal = 'Muhammad';
+//   String namaAKhir = "Alfianor Fitri";
+
+//   print(namaAwal);
+//   print(namaAKhir);
+// }
+
+
+// INI ADALAH INTERPOLATION STRING
+// void main(){
+//   String namaAwal = 'Muhammad';
+//   String namaAKhir = 'Alfianor Fitri';
+
+//   var gabungNama = '$namaAwal ${namaAKhir}';
+
+//   print(gabungNama);
+// }
+
+//INI ADALAH KARAKTER BACKSLASH
+// void main(){
+//   var text = 'ini adalah bahasa \'dart\' \$keren kan';
+
+//   print(text);
+// }
+
+//INI ADALAH PENGGABUNGAN STRING
+// void main(){
+//   String namaAwal = 'Muhammad';
+//   String namaAKhir = 'Alfianor Fitri';
+
+//   var nama1 = namaAwal + ' ' + namaAKhir;
+//   var nama2 = 'Muhammad' ' Alfianor' ' Fitri';
+
+//   print(nama1);
+//   print(nama2);
+// }
+
+
+//INI ADALAH MULTILINE STRING
+// void main(){
+//   String teksPanjang = ''' aku hari ini akan membuat sebuah paragraf yang panjang beuttt
+//   saking panjang nya ya gini udah berapa karakter ini ''';
+
+//   print(teksPanjang);
+// }
