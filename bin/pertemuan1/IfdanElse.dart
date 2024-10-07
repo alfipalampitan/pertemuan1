22d8fe32666177e6dbb0af2b0906564a5c06daa5
alfipalampitan <alfipalampitan123@gmail.com>
2024-10-07 13:48:14 +0800

    ini tugas untuk if dan else pa

---
+++ bin/pertemuan1/IfdanElse.dart
@@ -0,0 +1,31 @@
+//INI ADALAH CONTOH IF DAN ELSE
+// void main(){
+//   var nilaiTugas = 70;
+//   var nilaiAbsen = 80;
+
+//   if(nilaiTugas >= 75 && nilaiAbsen >= 75){
+//     print('kamu berhasil ges');
+//   }else{
+//     print('maaf ya kamu masih di sayang di sini');
+//   }
+// }
+
+//INI ADALAH CONTOH PENGGUNAAN ELSE IF
+// void main(){
+//   var nilaiTugas = 90;
+//   var nilaiAbsen = 90;
+
+//   if(nilaiTugas >= 80 && nilaiAbsen >= 80){
+//     print('nilai kamu = A');
+//   }else if(nilaiTugas >= 70 && nilaiAbsen >= 70){
+//     print('nilai kamu = B');
+//   }else if(nilaiTugas >= 60 && nilaiAbsen >= 60){
+//     print('nilai kamu = C');
+//   }else if(nilaiTugas >= 50 && nilaiAbsen >= 50){
+//     print('nilai kamu = D');
+//   }else if(nilaiTugas >= 40 && nilaiAbsen >= 40){
+//     print('nilai kamu = D');
+//   }else{
+//     print('nilai kamu = E');
+//   }
+// }
