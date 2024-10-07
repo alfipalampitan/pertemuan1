de258f5c2cf998cb5a82eff690f947d0bdd328ba
alfipalampitan <alfipalampitan123@gmail.com>
2024-10-07 16:45:43 +0800

    ini tugas untuk switchcase pa

---
+++ bin/pertemuan1/SwitchCase.dart
@@ -0,0 +1,19 @@
+//INI ADALAH CONTOH SWITCHCASE
+// void main(){
+//   var nilai = 'E';
+
+//   switch(nilai){
+//     case 'A':
+//     print('Keren Kamu lulus dengan Baik Sekali');
+//     break;
+//     case 'B':
+//     case 'C':
+//     print('kamu lulus dengan baik');
+//     break;
+//     case 'D':
+//     print('maaf kamu keknya belum bisa lulus');
+//     break;
+//     default:
+//     print('keknya salah jurusan deh kamu soalnya ga sesuai nih');
+//   }
+// }
