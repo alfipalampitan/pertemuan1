13270fbeb0df4b9239df91c34da5a3013f31406a
alfipalampitan <alfipalampitan123@gmail.com>
2024-10-27 08:40:00 +0800

    ini tugas nya pa kd sempat te commit kmrin

---
+++ bin/TugasOop/constructor.dart
@@ -0,0 +1,18 @@
+//Ini adalah contoh pembuatan constructor
+// class Person {
+
+//   String nama = 'Alfi';
+//   String? alamat;
+//   final String negara = 'indonesia';
+
+//   Person (String paramNama, String paramAlamat){
+//     nama = paramNama;
+//     alamat = paramAlamat;
+//   }
+// }
+
+// void main(){
+//   var person1 = Person("Muhammad Alfianor Fitri","Banjarmasin");
+//   print(person1.nama);
+//   print(person1.alamat);
+// }
