2968443dbe7743368806aeafd368f8f26ec755a5
alfipalampitan <alfipalampitan123@gmail.com>
2024-11-01 21:18:33 +0800

    Ini tugas nya pa

---
+++ bin/TugasOop/InitializerList.dart
@@ -0,0 +1,23 @@
+//Ini adalah contoh Initializer List
+// class Pengguna {
+//   String namaAwal = '';
+//   String namaAkhir = '';
+//   String fullnama = '';
+
+
+//   Pengguna(this.fullnama)
+//   : namaAwal = fullnama.split(" ")[0],
+//     namaAkhir = fullnama.split(" ")[1]{
+
+//       print('buat nama');
+
+//     }
+
+// }
+
+// void main(){
+//   var pengguna1 = Pengguna('Muhammad Alfianor Fitri');
+//   print(pengguna1.fullnama);
+//   print(pengguna1.namaAwal);
+//   print(pengguna1.namaAkhir);
+// }
